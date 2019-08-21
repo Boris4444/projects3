813f66da5b4c53ba0b7150cb7cdfc9a5f7687c3d
Boris H <bobo84@gmx.net>
2019-08-21 04:45:05 +0200

    Testklasse mit einem Testfall erstellt

---
+++ src/test/java/com/arvatoTestaufgabe3/FlearCalculationsTests.java
@@ -0,0 +1,32 @@
+package com.arvatoTestaufgabe3;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FlearCalculationsTests {
+
+	@Test
+	public void fourFlearFlow() throws Exception{
+		
+		Flear f1 = new Flear("Fipps", 20.0f, 10);
+		Flear f2 = new Flear("Friedolin", 200.0f, 10);
+		Flear f3 = new Flear("Fury", 10.0f, 30);
+		Flear f4 = new Flear("Fanta", 5.0f, 5);
+		
+		List<Flear> list = new ArrayList<Flear>();
+		list.add(f1);
+		list.add(f2);
+		list.add(f3);
+		list.add(f4);
+		
+		FlearCalculations fc = new FlearCalculations();
+		int result = fc.getOptimalValue(50f, list);		
+		System.out.println(result);
+		assertEquals(55, result);
+	}
+	
+}
